6dc9a0e5c749cff46d27678eea37d079a9726661
rmngqibisa <102858864+rmngqibisa@users.noreply.github.com>
2026-09-14 18:53:22 +0000

    Fix MSVC LNK1169 linker error and update rustls in lockfiles

--- server/build.rs
+++ server/build.rs
@@ -4,9 +4,5 @@ fn main() {
         println!("cargo:rustc-link-lib=c++");
     } else if target.contains("linux") {
         println!("cargo:rustc-link-lib=stdc++");
-    } else if target.contains("msvc") {
-        // Link dynamically against the MSVC C++ runtime library and Universal C Runtime library
-        println!("cargo:rustc-link-lib=msvcprt");
-        println!("cargo:rustc-link-lib=ucrt");
     }
 }
